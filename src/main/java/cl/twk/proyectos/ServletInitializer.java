6e3f0bbebd84e946afcf1d6c36f113fdda4e8d21
MatiasTWK <bxkick123@gmail.com>
2021-11-18 12:59:52 -0300

    carga inicial proyecto

---
+++ src/main/java/cl/twk/proyectos/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cl.twk.proyectos;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProyectosApplication.class);
+	}
+
+}
